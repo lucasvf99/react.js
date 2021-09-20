2b4de720d934772479fffd461a28c78cc6de2856
lucas fernandez <lucasvf4379@gmail.com>
2021-09-20 12:31:18 -0300

    nuevo repositorio

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/NavBar/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <h1>The Queen Accessories</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,44 @@
+import './NavBar.css';
+import corona from '../asset/corona.png' 
+//external componets
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+
+
+
+
+
+
+ const NavBar =() => {
+    return (
+        <AppBar position="static">
+          <Toolbar>
+          
+            <Typography variant="h6" >
+            <div className="conteiner_corona">
+                <img src={corona} alt="" />
+            </div>
+            
+            </Typography>
+                      
+                      <ul className ="conteiner_navbar">
+                      <li> <Button color="inherit"> Inicio</Button></li>
+                      <li> <Button color="inherit"> Productos</Button></li>
+                      <li> <Button color="inherit"> Preguntas</Button></li>
+                      <li> <Button color="inherit"> Contacto</Button></li>
+                      <li>  <Button color="inherit"> <AddShoppingCartIcon/></Button> </li>
+           
+                      </ul>
+               
+          </Toolbar>
+        </AppBar>
+    
+    );
+  }
+
+
+  export default NavBar ;
